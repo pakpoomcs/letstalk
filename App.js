abc99f26ea00bbfa9832ea3d2d17476df9e67fb5
Pakpoom Sungtong <pakpoom.cs@gmail.com>
2023-08-01 18:43:06 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import { useState } from 'react';
+
+
+const PlaceholderImage = 'https://docs.expo.dev/static/images/tutorial/background-image.png';
+import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import ImageViewer from './components/ImageViewer';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+
+    const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+
+    } else {
+      alert('You did not select any image.');
+    }
+
+    const onReset = () => {
+      setShowAppOptions(false);
+    };
+  
+    const onAddSticker = () => {
+      // we will implement this later
+    };
+  
+    const onSaveImageAsync = async () => {
+      // we will implement this later
+    };
+  
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+      <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +64,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+
 });
